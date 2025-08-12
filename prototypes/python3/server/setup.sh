e478f090c2a540326137e8b3cce87384bee1b10f
Sam Flannagan <svcflannagan@gmail.com>
2025-08-12 00:53:41 -0500

    Update setup.sh
    Undo changes

--- prototypes/python3/server/setup.sh
+++ prototypes/python3/server/setup.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Create virtual environment if it doesn't exist
 if [ ! -f $SCRIPT_DIR/venv ]; then
 	echo Creating virtual environment...
-	python -m venv venv #TODO
+	python3 -m venv venv
 fi
 
 # Activate virtual environment
