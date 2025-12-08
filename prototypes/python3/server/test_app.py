9d387ba0be70952e53f0574d13949078bd722c2a
Jonathan Wise <jonathan.wise@cesmii.org>
2025-12-08 16:07:01 -0500

    fix tests and qos0 mock data updater

--- prototypes/python3/server/test_app.py
+++ prototypes/python3/server/test_app.py
@@ -9,8 +9,14 @@ import asyncio
 
 
 class TestI3XEndpoints(unittest.TestCase):
-    def setUp(self):
-        self.client = TestClient(app)
+    @classmethod
+    def setUpClass(cls):
+        cls.client = TestClient(app)
+        cls.client.__enter__()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.client.__exit__(None, None, None)
 
     def test_namespaces_endpoint(self):
         """Test RFC 4.1.1 - Namespaces"""
@@ -21,55 +27,55 @@ class TestI3XEndpoints(unittest.TestCase):
 
     def test_object_types_endpoint(self):
         """Test RFC 4.1.3 - Object Types"""
-        response = self.client.get("/typeDefinitions")
+        response = self.client.get("/objecttypes")
         data = response.json()
 
         self.assertEqual(response.status_code, 200)
 
     def test_object_type_definition_endpoint(self):
         """Test RFC 4.1.2 - Object Type Definition"""
-        response = self.client.get("/objectType/machine-type-001")
+        response = self.client.get("/objecttypes/work-center-type")
         data = response.json()
 
         self.assertEqual(response.status_code, 200)
 
         # Test non-existent type
-        response = self.client.get("/objectType/non-existent")
+        response = self.client.get("/objecttypes/non-existent")
         self.assertEqual(response.status_code, 404)
 
     def test_instances_endpoint(self):
         """Test RFC 4.1.6 - Instances of an Object Type"""
-        response = self.client.get("/instances")
+        response = self.client.get("/objects")
         data = response.json()
 
         self.assertEqual(response.status_code, 200)
 
     def test_object_definition_endpoint(self):
         """Test RFC 4.1.8 - Object Definition"""
-        response = self.client.get("/object/machine-001")
+        response = self.client.get("/objects/pump-101")
         data = response.json()
 
         self.assertEqual(response.status_code, 200)
 
         # Test non-existent object
-        response = self.client.get("/object/non-existent")
+        response = self.client.get("/objects/non-existent")
         self.assertEqual(response.status_code, 404)
 
     def test_last_known_value_endpoint(self):
         """Test RFC 4.2.1.1 - Object Element LastKnownValue"""
-        response = self.client.get("/value/machine-001")
+        response = self.client.get("/objects/sensor-001/value")
         data = response.json()
 
         self.assertEqual(response.status_code, 200)
 
-        # Test with metadata
-        response = self.client.get("/value/machine-001?includeMetadata=true")
+        # Test with recurseDepth
+        response = self.client.get("/objects/pump-101/value?recurseDepth=1")
         data = response.json()
         self.assertEqual(response.status_code, 200)
 
     def test_hierarchical_relationships_endpoint(self):
-        """Test RFC 4.1.4 - Relationship Types - Hierarchical"""
-        response = self.client.get("/relationshipTypes/hierarchical")
+        """Test RFC 4.1.4 - Relationship Types"""
+        response = self.client.get("/relationshiptypes")
         data = response.json()
 
         self.assertEqual(response.status_code, 200)
@@ -77,14 +83,14 @@ class TestI3XEndpoints(unittest.TestCase):
     # TODO this probably belongs on the client side and is more than a unit test, placing here so I have a place to test QoS0
     def test_qos0_subscription_streaming(self):
         # Step 1: Create a QoS0 subscription
-        response = self.client.post("/subscribe", json={"qos": "QoS0"})
+        response = self.client.post("/subscriptions", json={"qos": "QoS0"})
         self.assertEqual(response.status_code, 200)
         subscription_id = response.json()["subscriptionId"]
         self.assertIsNotNone(subscription_id)
 
         # Step 2: Register monitored items and start streaming
-        url = f"/subscribe/{subscription_id}/register"
-        payload = {"elementIds": ["machine-001"]}
+        url = f"/subscriptions/{subscription_id}/objects"
+        payload = {"elementIds": ["sensor-001"]}
 
         # We will run the streaming request in a separate thread to allow timeout
         results = []
